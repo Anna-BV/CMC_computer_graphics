19d0cb88e67fa9bf664bfa7a5caecb6f57ae0bec
Anna Bryndina <58561289+Anna-BV@users.noreply.github.com>
2020-11-29 23:07:16 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -10,16 +10,19 @@ void processInput(GLFWwindow* window) { // обработка событий вводы
 // вершинный шейдер
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(0.5,0.5,0.5,1.0);\n"
 "}\0";
 // фрагментный шейдер
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;" // передано
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.2f, 0.1f, 0.3f, 0.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 int main(void)
