b999c8e3b8dc5ba8f9a39a929b92ed34c5194ae2
Anna Bryndina <58561289+Anna-BV@users.noreply.github.com>
2020-12-11 23:53:34 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,8 @@
-#define GLEW_STATIC
-//#include <GLEW/glew.h>
-#include <stb_image/stb_image.h>
 #include <GLAD/include/glad/glad.h>
 #include  <GLFW/glfw3.h>
 #include <string>
 #include <iostream>
-//#include <SOIL/SOIL.h>
+#include <stb_image/stb_image.h>
 
 
 #include "shader_h.h"
@@ -33,7 +30,7 @@ float deltaTime = 0.0f;//  Время, прошедшее между последним и текущим кадром
 float lastX = NWIDTH / 2.0;
 float lastY = NHEIGHT / 2.0;
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f); // 
+glm::vec3 lightPos(1.0f, 1.0f, 0.2f); // 
 int main(void)
 {
     if (!glfwInit()) {
@@ -67,29 +64,26 @@ int main(void)
         return -1;
     }
 
-    /*glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    } */
-
-    //glViewport(0, 0, NWIDTH, NHEIGHT);
+    glViewport(0, 0, NWIDTH, NHEIGHT);
+    // включаем тестирование трафаретов
     glEnable(GL_DEPTH_TEST);
+    //определяет функцию, используемую для сравнения
+    glDepthFunc(GL_LESS);
+    glEnable(GL_STENCIL_TEST);
+    // GL_NOTEQUAL	: Проходит, если входящее значение не равно установленному.
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    // три параметра, с помощью которых мы можем определить действия для каждого варианта
+    // GL_KEEP хранящееся в данный момент значение трафарета сохраняется
+    // GL_REPLACE трафаретное значение заменяется эталонным значением, установленным функцией glStencilFunc
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
-    Shader lightShader("light_vertex_shader.vs", "light_fragment_shader.vs"); // замена шейдеров
-    //std::cout << "myShader" << std::endl;
-    Shader myShader("all_light.vs", "all_light.fs");
-    //std::cout << "lightShader" << std::endl;
-    /*GLfloat vertices[] = { // ВЕРШИНЫ ТРЕУГОЛЬНИКОВ
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top
-    };*/
+    // для освещения
+    Shader lightShader("lightcub.vs", "lightcub.fs"); // замена шейдеров
+    Shader myShader("myshader.vs", "myshader.fs");
+    Shader shaderColor("vshader.vs", "color.fs");
 
-    GLfloat vertices[] = {
-        // нормали
+    // куб
+    float cubeVertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
      0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
      0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
@@ -132,115 +126,76 @@ int main(void)
     -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
-    // задаем положения кубов
-    glm::vec3 cubePositions[] = {
-    glm::vec3(0.0f,  0.0f,  0.0f),
-    glm::vec3(-1.5f, -2.2f, -2.5f),
-    glm::vec3(2.4f, -0.4f, -3.5f),
-    glm::vec3(-1.7f,  3.0f, -7.5f),
-    glm::vec3(1.3f, -2.0f, -2.5f),
-    glm::vec3(1.5f,  2.0f, -2.5f),
-    glm::vec3(1.5f,  0.2f, -1.5f),
-    glm::vec3(-1.3f,  1.0f, -1.5f)
+    // пол
+    float floorVertices[] = {
+         5.0f, -0.5f,  5.0f,  0.0f, 1.0f, 0.0f,   1.0f,  0.0f,
+        -5.0f, -0.5f,  5.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,   0.0f,  1.0f,
+
+         5.0f, -0.5f,  5.0f,  0.0f, 1.0f, 0.0f,   1.0f,  0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,   0.0f,  1.0f,
+         5.0f, -0.5f, -5.0f,  0.0f, 1.0f, 0.0f,   1.0f,  1.0f
     };
-    glm::vec3 pointLightPositions[] = {
-       glm::vec3(0.7f,  0.2f,  2.0f),
-       glm::vec3(2.3f, -3.3f, -4.0f),
-       glm::vec3(0.0f,  0.0f, -3.0f)
-    };
-    glm::vec3 pointLightColors[] = {
-          glm::vec3(0.2f, 0.2f, 0.6f),
-          glm::vec3(0.9f, 0.3f, 0.86f),
-          glm::vec3(0.9f, 0.4f, 0.9f),
-    };
+   
+    unsigned int  ourcubeVBO, ourcubeVAO;
+    glGenVertexArrays(1, &ourcubeVAO);
+    glGenBuffers(1, &ourcubeVBO);
 
+    glBindVertexArray(ourcubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, ourcubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
 
-    GLuint  VBO, containerVAO;
-    glGenVertexArrays(1, &containerVAO);
-    glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO);
-    // связываем объект вершинного массива
-
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-     //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glBindVertexArray(containerVAO);
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // TexCoord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
-    //glBindVertexArray(0);
+
+    
+
+    unsigned int floorVAO, floorVBO;
+    glGenVertexArrays(1, &floorVAO);
+    glGenBuffers(1, &floorVBO);
+    glBindVertexArray(floorVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), &floorVertices, GL_STATIC_DRAW);
+    //glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // Texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glBindVertexArray(0);
+
+
     // лампа
     unsigned int lightVAO;
     glGenVertexArrays(1, &lightVAO);
     glBindVertexArray(lightVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, ourcubeVBO);
     // настройка атрибутов
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(0));
     glEnableVertexAttribArray(0);
 
+    // загрузка текстур
+    // куб
+    unsigned int diffuseMap = loadTexture("serebro.jpg");
+    unsigned int specularMap = loadTexture("specular_serebro.jpg");
+    // пол
+    unsigned int diffFloorTexture = loadTexture("earth.jpg");
+    unsigned int specFloorTexture = loadTexture("specular_earth.jpg");
 
-    unsigned int diffuseMap = loadTexture("box.png");
-    unsigned int specularMap = loadTexture("container2_specular.png");
 
     myShader.Use();
     myShader.setInt("material.diffuse", 0);
     myShader.setInt("material.specular", 1);
-    /*GLuint texture1;
-    GLuint texture2;
-
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height;
-    unsigned char* image = SOIL_load_image("stone.png", &width, &height, 0, SOIL_LOAD_RGB);
-    if (image)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    image = SOIL_load_image("awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
-    if (image)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0); */
 
     while (!glfwWindowShouldClose(window))
     {
@@ -250,106 +205,106 @@ int main(void)
 
         processInput(window);
 
-        //glClearColor(1.0f, 0.89f, 0.92f, 1.0f);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        shaderColor.Use();
+        glm::mat4 projection = glm::perspective(camera.Zoom, (float)NWIDTH / (float)NHEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        shaderColor.setMat4("view", view);
+        shaderColor.setMat4("projection", projection);
+        glm::mat4 model = glm::mat4(1.0f);
 
         myShader.Use();
+        myShader.setMat4("view", view);
+        myShader.setMat4("projection", projection);
+        
+        // для освещения
         myShader.setVec3("viewPos", camera.Position);
         myShader.setFloat("material.shininess", 32.0f);
-        // направленный источник
-        myShader.setVec3("directLight.ambient", 0.0f, 0.0f, 0.0f);
-        myShader.setVec3("directLight.diffuse", 0.2f, 0.2f, 0.2f);
-        myShader.setVec3("directLight.specular", 0.5f, 0.5f, 0.5f);
-        myShader.setVec3("directLight.direction", -0.2f, -1.0f, -0.3f);
-        // точечный источник 1
-        myShader.setVec3("pointLight[0].position", pointLightPositions[0]);
-        myShader.setVec3("pointLight[0].ambient", pointLightColors[0].x * 0.1, pointLightColors[0].y * 0.1, pointLightColors[0].z * 0.1);
-        myShader.setVec3("pointLight[0].diffuse", pointLightColors[0].x, pointLightColors[0].y, pointLightColors[0].z);
-        myShader.setVec3("pointLight[0].specular", pointLightColors[0].x, pointLightColors[0].y, pointLightColors[0].z);
-        myShader.setFloat("pointLight[0].constant", 1.0f);
-        myShader.setFloat("pointLight[0].linear", 0.09);
-        myShader.setFloat("pointLight[0].quadratic", 0.08);
-        // точечный источник 2
-        myShader.setVec3("pointLight[1].position", pointLightPositions[1]);
-        myShader.setVec3("pointLight[1].ambient", pointLightColors[1].x, pointLightColors[1].y, pointLightColors[1].z);
-        myShader.setVec3("pointLight[1].diffuse", pointLightColors[1].x, pointLightColors[1].y, pointLightColors[1].z);
-        myShader.setVec3("pointLights[1].specular", pointLightColors[1].x, pointLightColors[1].y, pointLightColors[1].z);
-        myShader.setFloat("pointLight[1].constant", 1.0f);
-        myShader.setFloat("pointLight[1].linear", 0.09);
-        myShader.setFloat("pointLight[1].quadratic", 0.08);
+        // точечный источник
+        myShader.setVec3("light.position", lightPos);
+        myShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+        myShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        myShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        myShader.setFloat("light.constant", 1.0f);
+        myShader.setFloat("light.linear", 0.09f);
+        myShader.setFloat("light.quadratic", 0.032f);
+        //направленный источник
+        myShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        myShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+        myShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        myShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+ //----------------------- ЭФФЕКТ ОБВОДКИ------------------------------------------------------------
+        // проверяем, что буфер трафарета не обновляется во время рисования
+        glStencilMask(0x00);
+        // рисуем пол
+        glBindVertexArray(floorVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffFloorTexture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specFloorTexture);
+        myShader.setMat4("model", glm::mat4(1.0f));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
 
-        myShader.setVec3("pointLight[2].position", pointLightPositions[2]);
-        myShader.setVec3("pointLight[2].ambient", pointLightColors[2].x, pointLightColors[2].y, pointLightColors[2].z);
-        myShader.setVec3("pointLight[1].diffuse", pointLightColors[2].x, pointLightColors[2].y, pointLightColors[2].z);
-        myShader.setVec3("pointLights[1].specular", pointLightColors[2].x, pointLightColors[2].y, pointLightColors[2].z);
-        myShader.setFloat("pointLight[1].constant", 1.0f);
-        myShader.setFloat("pointLight[1].linear", 0.07);
-        myShader.setFloat("pointLight[1].quadratic", 0.05);
-
-        // прожектор
-        myShader.setVec3("spotLight.position", camera.Position);
-        myShader.setVec3("spotLight.direction", camera.Front);
-        myShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-        myShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-        myShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-        myShader.setFloat("spotLight.constant", 1.0f);
-        myShader.setFloat("spotLight.linear", 0.09);
-        myShader.setFloat("spotLight.quadratic", 0.032);
-        myShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(13.5f)));
-        myShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(17.0f)));
-
-
-
-        glm::mat4 view;
-        glm::mat4 projection;
-        projection = glm::perspective(camera.Zoom, (float)NWIDTH / (float)NHEIGHT, 0.1f, 100.0f);
-        view = camera.GetViewMatrix();
-        myShader.setMat4("projection", projection);
-        myShader.setMat4("view", view);
-
-        glm::mat4 model = glm::mat4(1.0f);
-        myShader.setMat4("model", model);
-
+        // 1 этап рендеринга: рисуем сами кубики 
+        glStencilFunc(GL_ALWAYS, 1, 0xFF); // все фрагменты должны пройти тест трафарета
+        glStencilMask(0xFF); // включаем запись в буфер трафарета
+        glBindVertexArray(ourcubeVAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, diffuseMap);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, specularMap);
 
-        glBindVertexArray(containerVAO);
-        for (unsigned int i = 0; i < 8; i++) {
-
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-            myShader.setMat4("model", model);
-            //glBindVertexArray(containerVAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        }
-
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+        myShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+        myShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // 2 подход рендеринга: увеличим кубики,отключив запись по трафарету
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF); // чтобы убедиться, что визуализируем только те конт, которые не = 1
+        glStencilMask(0x00); // отключаем запись в буфер трафарета
+        glDisable(GL_DEPTH_TEST);
+        shaderColor.Use();
+        float scale = 1.1;
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+        shaderColor.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+        shaderColor.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glStencilMask(0xFF);
+        glStencilFunc(GL_ALWAYS, 0, 0xFF);
+        glEnable(GL_DEPTH_TEST);
+        
+        // источник света
         lightShader.Use();
         lightShader.setMat4("projection", projection);
         lightShader.setMat4("view", view);
-       
-        
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        lightShader.setMat4("model", model);
         glBindVertexArray(lightVAO);
-        for (int i = 0; i < 3; i++) {
-            lightShader.setVec3("colorpop", pointLightColors[i]);
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, pointLightPositions[i]);
-            model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-            lightShader.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+       
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    glDeleteVertexArrays(1, &containerVAO);
+    glDeleteVertexArrays(1, &ourcubeVAO);
+    glDeleteBuffers(1, &ourcubeVBO);
     glDeleteVertexArrays(1, &lightVAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &floorVAO);
+    glDeleteBuffers(1, &floorVBO);
+    glDeleteVertexArrays(1, &lightVAO);
     glfwTerminate();
     return 0;
 }
@@ -357,7 +312,7 @@ int main(void)
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    //std::cout << key << std::endl;
+   
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
@@ -375,7 +330,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 void update_camera() {
-    GLfloat cameraSpeed = 0.5 * deltaTime;
+    float cameraSpeed = 0.5 * deltaTime;
     if (keys[GLFW_KEY_W]) {
         camera.Keyboard(FORWARD, deltaTime);
     }
@@ -400,8 +355,8 @@ void mouse_moving(GLFWwindow* window, double xpos, double ypos) { // xpos,ypos -
         firstMouse = false;
     }
 
-    GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos; // обратный порядок вычитания потому что оконные Y-координаты возрастают с верху вниз 
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // обратный порядок вычитания потому что оконные Y-координаты возрастают с верху вниз 
 
     lastX = xpos;
     lastY = ypos;
